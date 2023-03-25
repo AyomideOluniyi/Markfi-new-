7b327b6deb0c9325334a2c6ea9407c40a0057a4e
Jessica Excell <JE398@canterbury.ac.uk>
2023-03-25 20:53:27 +0000

    Created an incorrect page to match the correct page

---
+++ Markfi/Markfi/Views/IncorrectAnswer.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Markfi.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IncorrectAnswer : ContentPage
+	{
+		public IncorrectAnswer ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
